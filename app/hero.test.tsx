a5e4cbd2109acd2b039ba6ec35bedaa193bef856
ryo-tsukada <r.tsukada14@gmail.com>
2024-05-15 09:31:57 +0900

    トップページからのリンクを追加する
    `hero.tsx` にリンクを追加した。`Get Started` ボタンをクリックすると `/explore` に遷移するようにした。

--- app/hero.test.tsx
+++ app/hero.test.tsx
@@ -1,15 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Hero from './hero';
+import mockRouter from 'next-router-mock';
+import userEvent from '@testing-library/user-event';
+import { ReactNode } from 'react';
+
+jest.mock('next/link', () => {
+  interface MockLinkProps {
+    children: ReactNode;
+    href: string;
+  }
+
+  const MockLink = ({ children, href }: MockLinkProps) => {
+    return <a href={href} onClick={() => mockRouter.push(href)}>{children}</a>;
+  };
+  MockLink.displayName = 'MockLink';
+  return MockLink;
+});
 
 describe('Hero component', () => {
+  const originalError = console.error;
+  beforeAll(() => {
+    console.error = (...args) => {
+      if (/Not implemented: navigation/.test(args[0])) {
+        return;
+      }
+      originalError.call(console, ...args);
+    };
+  });
+
+  afterAll(() => {
+    console.error = originalError;
+  });
+
   beforeEach(() => {
+    mockRouter.setCurrentUrl('/');
     render(<Hero />);
   });
 
-  test ('renders Hero component', () => {
+  test('renders Hero component', () => {
     const heroText = screen.getByText('Art Timeline');
     expect(heroText).toBeInTheDocument();
-  })
+  });
 
   test('renders description', () => {
     const descriptionText = screen.getByText(/Explore the Art of Time: Journey through centuries of creativity, from ancient masterpieces to modern expressions. Discover how art has shaped history and continues to inspire the world today. Dive into the 'Art Timeline' and let each stroke tell its story./i);
@@ -17,7 +48,18 @@ describe('Hero component', () => {
   });
 
   test('renders get started button', () => {
-    const getStartedButton = screen.getByRole('button', { name: /get started/i });
+    const getStartedButton = screen.getByRole('link', { name: /get started/i });
     expect(getStartedButton).toBeInTheDocument();
   });
-})
+
+  test('navigates to /explore when get started button is clicked', async () => {
+    const getStartedButton = screen.getByRole('link', { name: /get started/i });
+
+    await userEvent.click(getStartedButton);
+
+    await waitFor(() => {
+      expect(mockRouter).toMatchObject({ pathname: '/explore' });
+    });
+  });
+});
+
