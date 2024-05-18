d557f7560b41cbf5912067f1466f28c4ee609fd3
ryo-tsukada <r.tsukada14@gmail.com>
2024-05-18 20:14:41 +0900

    navbarから`/`に遷移するリンクを追加する
    - navbarコンポーネントに`/`に遷移するリンクを追加する
    - テストを追加する

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <>
-      <div className="navbar bg-base-100">
-        <a className="btn btn-ghost text-xl">Art Timeline</a>
+    <div className="navbar bg-base-100">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <p className="text-xl font-bold">Art Timeline</p>
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
