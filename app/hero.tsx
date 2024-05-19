edd5a33d43d4c0ef918d5349b6e6153befe8029e
ryo-tsukada <r.tsukada14@gmail.com>
2024-05-19 20:47:02 +0900

    トップページにレスポンシブデザインを適用する
    - トップページのヒーローセクションにレスポンシブデザインを適用
    - ヒーローセクションの高さを画面の高さから4remを引いた高さに変更
    - ヒーローセクションのコンテンツを中央に配置
    - ヒーローセクションのコンテンツのテキストサイズをレスポンシブに変更
    - ヒーローセクションのボタンの幅をレスポンシブに変更
    - ページ全体をフレックスコンテナに変更
    - メインコンテンツをフレックスアイテムに変更
    - メインコンテンツを中央に配置

--- app/hero.tsx
+++ app/hero.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className="hero min-h-screen relative">
+    <div className="hero relative w-full style={{ height: 'calc(100vh - 4rem)' }">
       <Image
         src="https://images.metmuseum.org/CRDImages/ep/web-large/DT1567.jpg"
         width={1920}
@@ -12,11 +12,11 @@ export default function Navbar() {
       />
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Art Timeline</h1>
-          <p className="mb-5">Explore the Art of Time: Journey through centuries of creativity, from ancient masterpieces to modern expressions. Discover how art has shaped history and continues to inspire the world today. Dive into the 'Art Timeline' and let each stroke tell its story.</p>
+        <div className="max-w-md mx-auto">
+          <h1 className="mb-5 text-5xl sm:text-4xl md:text-5xl font-bold">Art Timeline</h1>
+          <p className="mb-5 text-sm sm:text-base md:text-lg">Explore the Art of Time: Journey through centuries of creativity, from ancient masterpieces to modern expressions. Discover how art has shaped history and continues to inspire the world today. Dive into the 'Art Timeline' and let each stroke tell its story.</p>
           <Link href='/explore'>
-            <button className="btn btn-primary" aria-current="page">
+            <button className="btn btn-primary w-full sm:w-auto h-10 sm:h-12 md:h-14 lg:h-16" aria-current="page">
                 Get Started
             </button>
           </Link>
